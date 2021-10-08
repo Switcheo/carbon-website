e69c727894599be6dadc54c120cb8bad4862af84
Eugene <eugene.lim@switcheo.network>
2021-10-08 16:29:53 +0800

    update partner logos

--- src/@carbon-info/assets/logos/index.ts
+++ src/@carbon-info/assets/logos/index.ts
@@ -5,7 +5,7 @@ import { default as DACMLogo } from "./DACM.svg";
 import { default as DefianceCapitalLogo } from "./DefianceCapital.svg";
 import { default as DefiCapitalLogo } from "./DefiCapital.svg";
 import { default as DuskLogo } from "./Dusk.svg";
-import { default as EverStakeLogo } from "./Everstake.svg";
+import BlockHunterLogo from "./BlockHuntersLogo.png";
 import { default as HoubiLogo } from "./Houbi.svg";
 import { default as NeoLogo } from "./Neo.svg";
 import { default as NGCLogo } from "./NGC.svg";
@@ -15,6 +15,6 @@ import { default as StakeWithUsLogo } from "./Stakewithus.svg";
 import { default as ZilliqaLogo } from "./Zilliqa.svg";
 
 export const allPartnersLogo = [ ThreeACLogo, DefianceCapitalLogo, NGCLogo, NeoLogo, DefiCapitalLogo, PolyNetworkLogo, ZilliqaLogo, DACMLogo, DuskLogo, 
-  HoubiLogo, StakeWithUsLogo, O3LabsLogo, EverStakeLogo, BoltLogo];
+  HoubiLogo, StakeWithUsLogo, O3LabsLogo, BlockHunterLogo, BoltLogo];
   
 
