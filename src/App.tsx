c1cd5c2a4546c6f8ede3b2684be1a7e33083d241
Eugene <eugene.lim@switcheo.network>
2021-10-22 16:13:19 +0800

    remove stats section from lazy loading, add back animation

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import {
 
 import { ContentfulClient, ContentfulProvider } from "react-contentful";
 import { GoogleAnalytics } from "@carbon-info/components";
+import { DecentralizedStats } from "@carbon-info/views";
 const contentfulClient: any = ContentfulClient({
   accessToken: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN!,
   space: process.env.REACT_APP_CONTENTFUL_SPACE_ID!,
@@ -21,7 +22,6 @@ const FeatureCard = React.lazy(() => import("@carbon-info/views/FeatureCard/Feat
 const HeroImage = React.lazy(() => import("@carbon-info/views/HeroImage/HeroImage"));
 const IntroPage = React.lazy(() => import("@carbon-info/views/IntroPage/IntroPage"));
 const UtilitySection = React.lazy(() => import("@carbon-info/views/UtilitySection/UtilitySection"));
-const DecentralizedStats = React.lazy(() => import("@carbon-info/views/DecentralizedStats/DecentralizedStats"));
 const Permissionless = React.lazy(() => import("@carbon-info/views/Permissionless/Permissionless"));
 const Community = React.lazy(() => import("@carbon-info/views/Community/Community"));
 const Partnership = React.lazy(() => import("@carbon-info/views/Partnership/Partnership"));
