6c8261511969db58dd33057d0c0df080a5a4fed4
Eugene <eugene.lim@switcheo.network>
2021-11-12 11:03:01 +0800

    fix animation timing, removed unused css

--- src/@carbon-info/views/HeroImage/HeroImage.tsx
+++ src/@carbon-info/views/HeroImage/HeroImage.tsx
@@ -1,11 +1,8 @@
-// import CarbonStructureSphereBg from "@carbon-info/assets/animated/carbonBgSphere.png";
 import { Box, makeStyles, Theme, useMediaQuery, useTheme } from "@material-ui/core";
 import React, { useRef, useState } from "react";
 import { useInView } from "react-intersection-observer";
-// import clsx from "clsx";
 import heroSVGAnimationStart from "@carbon-info/assets/animated/heroSVGAnimationStart.json";
 import heroSVGAnimationEnd from "@carbon-info/assets/animated/heroSVGAnimationEnd.json";
-// import { Parallax, Background } from "react-parallax";
 import useInterval from "@carbon-info/hooks/useInterval";
 import Lottie from "react-lottie";
 import { FadeAndSlide } from "@carbon-info/components";
@@ -16,9 +13,9 @@ const HeroImage: React.FC = () => {
   const isMobile = useMediaQuery(theme.breakpoints.down("xs"));
   const anim1Ref = useRef<HTMLObjectElement | null>(null);
   const anim2Ref = useRef<HTMLObjectElement | null>(null);
-  const [step1, setStep1] = useState(false);
+  const [startEndAnimation, setStartEndAnimation] = useState(false);
 
-  const defaultOptions = {
+  const AniStartOptions = {
     loop: false,
     autoplay: false,
     animationData: heroSVGAnimationStart,
@@ -27,7 +24,7 @@ const HeroImage: React.FC = () => {
     },
   };
 
-  const defaultOptions2 = {
+  const AniEndOptions = {
     loop: true,
     autoplay: false,
     animationData: heroSVGAnimationEnd,
@@ -41,9 +38,9 @@ const HeroImage: React.FC = () => {
     if (!anim1Ref.current || !anim2Ref.current) return;
 
     setTimeout(() => {
-      setStep1(true);
-    }, 2000);
-  }, step1 ? null : 300);
+      setStartEndAnimation(true);
+    }, 1800);
+  }, startEndAnimation ? null : 300);
 
   const { ref, inView } = useInView({
     /* Optional options */
@@ -58,42 +55,22 @@ const HeroImage: React.FC = () => {
           <div id="anim" className={classes.anim}>
             <div ref={anim1Ref} style={{
               position: "absolute", zIndex: 33,
-              opacity: (inView && !step1) ? 1 : 0,
+              opacity: (inView && !startEndAnimation) ? 1 : 0,
             }}>
               <Lottie
                 isPaused={!inView}
-                options={defaultOptions}
+                options={AniStartOptions}
               />
             </div>
             <div ref={anim2Ref} style={{
-              position: "absolute", zIndex: 33, opacity: (inView && step1) ? 1 : 0,
+              position: "absolute", zIndex: 33, opacity: (inView && startEndAnimation) ? 1 : 0,
             }}>
               <Lottie
-                isPaused={!inView && !step1}
-                options={defaultOptions2} />
+                isPaused={!(inView && startEndAnimation)}
+                options={AniEndOptions} />
             </div>
           </div>
         </FadeAndSlide>
-        {/* <Parallax blur={10} strength={isMobile ? 80 : 120}>
-          <Background>
-            <img src={CarbonStructureSphereBg} alt="hero" className={clsx(classes.sphere, { open: isMobile ? inView && step1 : inView })} />
-          </Background>
-          <div className="container">
-            <div id="anim" className={classes.anim}>
-              <object ref={anim1Ref}
-                className={clsx(classes.svg, { hidden: !inView }, { step1 }, { step2 })}
-                id="anim1" data={heroSVGAnimationStart}
-                type="image/svg+xml">
-              </object>
-              <object ref={anim2Ref}
-                className={clsx(classes.svg, { hidden: true }, { anim2Step1: step1 || !inView }, { anim2Step2: step2 && inView })}
-                id="anim2"
-                data={heroSVGAnimationEnd}
-                type="image/svg+xml">
-              </object>
-            </div>
-          </div>
-        </Parallax> */}
       </Box>
     </div>
   );
@@ -102,25 +79,6 @@ const HeroImage: React.FC = () => {
 export default HeroImage;
 
 const useStyles = makeStyles((theme: Theme) => ({
-  svg: {
-    left: 0,
-    position: "absolute",
-    "&.hidden": {
-      opacity: 0,
-    },
-    "&.anim2Step1": {
-      opacity: 0,
-    },
-    "&.anim2Step2": {
-      opacity: 1,
-    },
-    "&.step1": {
-      opacity: 1,
-    },
-    "&.step2": {
-      opacity: 0,
-    },
-  },
   anim: {
     maxHeight: 800,
     height: "50vw",
@@ -131,18 +89,6 @@ const useStyles = makeStyles((theme: Theme) => ({
     },
     [theme.breakpoints.down("sm")]: {
       height: "80vw",
-    },
-  },
-  animationEnd: {
-    opacity: 0,
-    "&.open": {
-      opacity: 1,
-    },
-  },
-  animation: {
-    opacity: 0,
-    "&.open": {
-      opacity: 1,
     },
   },
   carbonStructure: {
@@ -160,25 +106,6 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
   heroSVG: {
     width: "100%",
-  },
-  sphere: {
-    width: "60%",
-    position: "absolute",
-    top: 116,
-    left: "67%",
-    opacity: 0,
-    transform: "scale(0.75)",
-    transition: "all ease-in 0.5s",
-    [theme.breakpoints.down("sm")]: {
-      top: 86,
-    },
-    [theme.breakpoints.down("xs")]: {
-      top: 46,
-    },
-    "&.open": {
-      opacity: 1,
-      transform: "scale(1)",
-    },
   },
 }));
 
