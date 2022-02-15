aff6fec658d6b76a1e25ce8fbbccede2884f6660
eugenelhj <81943900+eugenelhj@users.noreply.github.com>
2022-02-15 17:20:36 +0800

    Feat/change carbon logo (#9)
    * change carbon logo
    
    * adjustment to svg margin
    
    * update carbon logo
    
    * adjust svg size

--- src/@carbon-info/views/Community/Community.tsx
+++ src/@carbon-info/views/Community/Community.tsx
@@ -128,6 +128,7 @@ const useStyles = makeStyles((theme: Theme) => ({
       opacity: 1,
       transform: "translate(0px,0px) scale(1)",
     },
+    margin: "-3rem 0px 0px -5rem",
     [theme.breakpoints.down("md")]: {
       position: "relative",
       width: "100%",
@@ -136,7 +137,12 @@ const useStyles = makeStyles((theme: Theme) => ({
     [theme.breakpoints.down(1100)]: {
       position: "relative",
       width: "100%",
-      margin: "-10rem 0px 0px -2rem",
+      margin: "-1rem 0px 0px -2rem",
+    },
+    [theme.breakpoints.down(1060)]: {
+      position: "relative",
+      width: "100%",
+      margin: "-5rem 0px 0px -2rem",
     },
     [theme.breakpoints.down(961)]: {
       position: "relative",
@@ -144,12 +150,12 @@ const useStyles = makeStyles((theme: Theme) => ({
       margin: "-1rem 0px 0px -2rem",
     },
     [theme.breakpoints.down("sm")]: {
-      margin: 0,
       position: "relative",
       width: "57%",
+      margin: "0rem 0px 0px 0rem",
     },
     [theme.breakpoints.down("xs")]: {
-      margin: 0,
+      margin: "4rem 0px -2rem 0rem",
       position: "relative",
       width: "80%",
     },
