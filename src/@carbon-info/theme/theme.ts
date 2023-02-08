f2b6eec2d044cf1af2a018a3da1d23484e174bf8
Sharmaine <sharmainec@u.nus.edu>
2023-02-08 16:12:07 +0800

    Update theme and typography

--- src/@carbon-info/theme/theme.ts
+++ src/@carbon-info/theme/theme.ts
@@ -1,88 +1,86 @@
 import { createTheme } from "@material-ui/core";
 
 export const theme = createTheme({
-    palette: {
-      common: {
-        white: "#FFFFFF",
-        black: "#161515",
-      },
-      background: {
-        default: "#161515",
-        paper: "#161515",
-      },
-      primary: {
-        light: "#8CF2FD",
-        main: "#74E8E8",
-        dark: "#4FC1C0",
-      },
-      secondary: {
-        light: "#6B99C3",
-        main: "#5389BA",
-        dark: "#3A78B1",
-      },
-      text: {
-        primary: "#FFFFFF",
-      },
-      divider: "rgba(255,255,255,0.4)",
+  palette: {
+    common: {
+      white: "#FFFFFF",
+      black: "#000000",
     },
-    typography: {
-      fontFamily: "TyrosPro, SourceSansPro, SourceSansPro-Light, SourceSansPro-Bold, TyrosPro-Light, TyrosPro-SemiBold",
-      h1: {
-        fontWeight: 300,
-        fontSize: "4.375rem",
-        lineHeight: "4.375rem",
-        letterSpacing: "-0.063rem",
-      },
-      h2: {
-        fontWeight: 300,
-        fontSize: "3.438rem",
-        lineHeight: "3.953rem",
-        letterSpacing: "-0.063rem",
-      },
-      h3: {
-        fontWeight: 500,
-        fontSize: "2.5rem",
-        lineHeight: "63.25px",
-        letterSpacing: "-0.063rem",
-      },
-      h4: {
-        fontWeight: 700,
-        fontSize: "1.25rem",
-        lineHeight: "1.188rem",
-        letterSpacing: "4",
-      },
-      subtitle1: {
-        fontFamily: "SourceSansPro-Light",
-        fontWeight: 300,
-        fontSize: "2.125rem",
-        lineHeight: "3.018rem",
-        letterSpacing: "-0.063rem",
-      },
-      subtitle2: {
-        fontFamily: "SourceSansPro-Light",
-        fontWeight: 300,
-        fontSize: "2.725rem",
-        lineHeight: "3.018rem",
-        letterSpacing: "-0.063rem",
-      },
-      body1: {
-        fontFamily: "SourceSansPro-Light",
-        fontWeight: 200,
-        fontSize: "1.438rem",
-        lineHeight: "2.041rem",
-        letterSpacing: "-0.063rem",
-      },
-      body2: {
-        fontFamily: "SourceSansPro",
-        fontWeight: 400,
-        fontSize: "1.438rem",
-        lineHeight: "2.041rem",
-        letterSpacing: "-1%",
-      },
-      button: {
-        fontWeight: 700,
-        fontSize: "1.5rem",
-        lineHeight: "2.13rem",
-      },
+    background: {
+      default: "#161515",
+      paper: "#161515",
     },
+    primary: {
+      light: "#31D8D6", // highlight
+      main: "#74E8E8", // branding
+      dark: "#0ADCB6", // accents/green
+    },
+    secondary: {
+      light: "#6B99C3",
+      main: "#5389BA",
+      dark: "#3A78B1",
+    },
+    text: {
+      primary: "#FFFFFF",
+      secondary: "#B0B0B0",
+      disabled: "#5C5C5C",
+      hint: "#989898",
+    },
+    divider: "rgba(255,255,255,0.4)",
+  },
+  typography: {
+    fontFamily: "TyrosPro, SourceSansPro, SourceSansPro-Light, SourceSansPro-Bold, TyrosPro-Light, TyrosPro-SemiBold",
+    h1: { // XLarge 64px
+      fontFamily: "TyrosPro",
+      fontWeight: 900,
+      fontSize: "4rem",
+      lineHeight: "5.5rem",
+      letterSpacing: "-0.02rem",
+    },
+    h2: { // Large 48px
+      fontFamily: "TyrosPro",
+      fontWeight: 600,
+      fontSize: "3rem",
+      lineHeight: "4.125rem",
+      letterSpacing: "-0.02rem",
+    },
+    h3: { // 36px
+      fontFamily: "TyrosPro",
+      fontWeight: 600,
+      fontSize: "2.25rem",
+      lineHeight: "3.125rem",
+      letterSpacing: 0,
+    },
+    h4: { // Regular 24px/CTA & Title
+      fontFamily: "TyrosPro",
+      fontWeight: 700,
+      fontSize: "1.5rem",
+      lineHeight: "2.062rem",
+      letterSpacing: "-0.02rem",
+    },
+    h5: { // Mobile/Header 16px
+      fontFamily: "TyrosPro",
+      fontWeight: 700,
+      fontSize: "1rem",
+      lineHeight: "1.375rem",
+    },
+    body1: { // Regular 24px/Body
+      fontFamily: "TyrosPro",
+      fontWeight: 500,
+      fontSize: "1.5rem",
+      lineHeight: "2.062rem",
+      letterSpacing: 0,
+    },
+    body2: { // Small 20px/Body
+      fontFamily: "TyrosPro",
+      fontWeight: 400,
+      fontSize: "1.25rem",
+      lineHeight: "1.719rem",
+    },
+    button: {
+      fontWeight: 700,
+      fontSize: "1.5rem",
+      lineHeight: "2.13rem",
+    },
+  },
 });
