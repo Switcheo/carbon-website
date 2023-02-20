e5045baede9844848fc47defc30967a34a0441b5
Sharmaine <sharmainec@u.nus.edu>
2023-02-20 17:44:29 +0800

    Fix data table

--- src/@carbon-info/views/Data/Data.tsx
+++ src/@carbon-info/views/Data/Data.tsx
@@ -1,5 +1,5 @@
 import { FadeAndSlide } from "@carbon-info/components";
-import { Box, makeStyles, Table, TableBody, TableCell, TableRow, Theme, Typography } from "@material-ui/core";
+import { Divider, Grid, makeStyles, Paper, Theme, Typography, useMediaQuery, useTheme } from "@material-ui/core";
 import clsx from "clsx";
 import React from "react";
 import { useInView } from "react-intersection-observer";
@@ -11,11 +11,14 @@ interface DataInfo {
 
 const Data: React.FC = () => {
   const classes = useStyles();
+  const theme = useTheme();
   const { ref, inView } = useInView({
     /* Optional options */
     threshold: 0.4,
     triggerOnce: true,
   });
+
+  const widthXs = useMediaQuery(theme.breakpoints.only("xs"));
 
   const tableInfo: DataInfo[] = [{
     value: "157,000,000+",
@@ -49,30 +52,17 @@ const Data: React.FC = () => {
         </Typography>
       </FadeAndSlide>
       <FadeAndSlide visible={inView}>
-        <Table className={clsx(classes.dataTable, { open: inView })}>
-          <TableBody>
-            <TableRow>
-              {tableInfo.slice(0, 3).map((item: DataInfo, index) => (
-                <TableCell className={classes.tableCell} key={item.description.replace(" ", "-")}>
-                  <Box className={index === 0 ? "" : classes.dataBox} >
-                    <Typography variant="h3" color="textPrimary" align="center">{item.value}</Typography>
-                    <Typography variant="body2" color="textSecondary" align="center">{item.description}</Typography>
-                  </Box>
-                </TableCell>
-              ))}
-            </TableRow>
-            <TableRow>
-              {tableInfo.slice(3, 6).map((item: DataInfo, index) => (
-                <TableCell className={classes.tableCell} key={item.description.replace(" ", "-")}>
-                  <Box className={index === 0 ? "" : classes.dataBox}>
-                    <Typography variant="h3" color="textPrimary" align="center">{item.value}</Typography>
-                    <Typography variant="body2" color="textSecondary" align="center">{item.description}</Typography>
-                  </Box>
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableBody>
-        </Table>
+        <Grid container item xs={8} sm={12} spacing={0} justifyContent="center" className={clsx(classes.dataTable, { open: inView })}>
+          {tableInfo.map((item: DataInfo) => (
+            <Grid item xs={12} sm={4} xl={2} key={item.description.replace(" ", "-")}>
+              <Paper className={classes.dataBox} elevation={0}>
+                <Typography variant="h3" color="textPrimary" align="center">{item.value}</Typography>
+                <Typography variant="body2" color="textSecondary" align="center">{item.description}</Typography>
+              </Paper>
+              {widthXs && <Divider className={classes.mobileDivider} />}
+            </Grid>
+          ))}
+        </Grid>
       </FadeAndSlide>
     </div >
   );
@@ -87,7 +77,7 @@ const useStyles = makeStyles((theme: Theme) => ({
     justifyContent: "center",
     alignItems: "center",
     position: "relative",
-    marginTop: "31.25rem",
+    marginTop: "10rem",
     paddingBottom: "10rem",
   },
   headerText: {
@@ -100,10 +90,8 @@ const useStyles = makeStyles((theme: Theme) => ({
     },
   },
   dataTable: {
-    width: "1113px",
-    height: "272px",
     marginTop: "10rem",
-    background: "rgba(24, 35, 35, 0.8)",
+    background: "linear-gradient(0deg, rgba(24, 35, 35, 0.8), rgba(24, 35, 35, 0.8))",
     borderWidth: "0px 4px",
     borderStyle: "solid",
     borderColor: "#74E8E8",
@@ -114,11 +102,43 @@ const useStyles = makeStyles((theme: Theme) => ({
     "&.open": {
       opacity: 1,
     },
-  },
-  tableCell: {
-    border: "none",
+    [theme.breakpoints.only("xl")]: {
+      width: "1823px",
+      "& > div:not(:first-child)": {
+        "& > div": {
+          borderLeft: `2px solid ${theme.palette.primary.dark}`,
+        },
+      },
+    },
+    [theme.breakpoints.between("sm", "lg")]: {
+      width: "100%",
+      "& > div:not(:nth-child(3n+1))": {
+        "& > div": {
+          borderLeft: `2px solid ${theme.palette.primary.dark}`,
+        },
+      },
+    },
+    [theme.breakpoints.only("xs")]: {
+      margin: "10rem auto 0",
+      "& > div:last-child > hr": {
+        display: "none",
+      },
+    },
   },
   dataBox: {
-    borderLeft: `2px solid ${theme.palette.primary.dark}`,
+    margin: theme.spacing(4, 0),
+    borderRadius: 0,
+    backgroundColor: "transparent",
+    [theme.breakpoints.only("xs")]: {
+      margin: theme.spacing(2, 0),
+    },
+  },
+  mobileDivider: {
+    width: "20px",
+    height: "2px",
+    backgroundColor: "rgba(116, 232, 232, 0.75)",
+    boxShadow: "0px 0px 8px rgba(0, 242, 199, 0.33)",
+    borderRadius: "4px",
+    margin: "auto",
   },
 }));
