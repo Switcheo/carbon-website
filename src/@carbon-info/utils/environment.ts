4108ecf6c72476d48a8182721313acbda4316f2e
Sharmaine <sharmainec@u.nus.edu>
2023-03-24 13:15:26 +0800

    Fix mobile roadmap

--- src/@carbon-info/utils/environment.ts
+++ src/@carbon-info/utils/environment.ts
@@ -1,13 +1,6 @@
 import { useMediaQuery, useTheme } from "@material-ui/core";
 
 /**
- * checks if browser is Safari/iOS
- */
-export const isMobileSafari = () => {
-  return !!(navigator.userAgent.indexOf("iPhone") > -1);
-};
-
-/**
  * checks if browser is Mozilla Firefox
 */
 export const isFirefox = (): boolean => {
