4c2548d5873b2889a14666c8d2907fec0babedd2
rushilramesh <rushilramesh@gmail.com>
2023-08-17 14:25:45 +0800

    Fix scroll prompt on intro page

--- src/@carbon-info/views/IntroPage/IntroPage.tsx
+++ src/@carbon-info/views/IntroPage/IntroPage.tsx
@@ -29,17 +29,32 @@ const IntroPage: React.FC = () => {
   };
 
   const scrollRef = React.useRef<HTMLDivElement>(null);
+  const altCarbonRef = React.useRef<HTMLDivElement>(null);
   const [height, setHeight] = React.useState(0);
   React.useEffect(() => {
     if (scrollRef.current) {
-      const homeHeight = scrollRef.current.parentElement?.parentElement?.parentElement?.offsetHeight ?? 0;
-      const navBarHeight = scrollRef.current.parentElement?.parentElement?.parentElement?.offsetTop ?? 0;
+      const homeHeight =
+        scrollRef.current.parentElement?.parentElement?.parentElement
+          ?.offsetHeight ?? 0;
+      const navBarHeight =
+        scrollRef.current.parentElement?.parentElement?.parentElement
+          ?.offsetTop ?? 0;
       setHeight(homeHeight + navBarHeight);
     }
   }, []);
 
   const goToData = () => {
     window.scrollTo(0, height);
+  };
+
+  const goToAltCarbon = () => {
+    window.scrollTo({
+      left: 0,
+      top:
+        document.documentElement.getBoundingClientRect().height ??
+        document.body.scrollHeight,
+      behavior: "smooth",
+    });
   };
 
   return (
@@ -52,14 +67,26 @@ const IntroPage: React.FC = () => {
         />
         <div className={classes.headerContainer}>
           <FadeAndSlide visible={inView} transform={[0, -20]}>
-            <Typography variant="body1" color="textSecondary" className={classes.bodyTypography}>
+            <Typography
+              variant="body1"
+              color="textSecondary"
+              className={classes.bodyTypography}
+            >
               MEET CARBON
             </Typography>
           </FadeAndSlide>
-          <Typography color="textPrimary" variant="h1" className={clsx(classes.mainTitle, "animate__animated animate__slideInUp", { open: inView })} >
+          <Typography
+            color="textPrimary"
+            variant="h1"
+            className={clsx(
+              classes.mainTitle,
+              "animate__animated animate__slideInUp",
+              { open: inView }
+            )}
+          >
             The Core of
             <br />
-            <span className={clsx(classes.highlightedText, { open: inView })} >
+            <span className={clsx(classes.highlightedText, { open: inView })}>
               Decentralized
               <br />
             </span>
@@ -68,19 +95,43 @@ const IntroPage: React.FC = () => {
             </span>
           </Typography>
           <FadeAndSlide visible={inView}>
-            <Typography color="textSecondary" variant="body1" className={clsx(classes.subtitle, "animate__animated animate__slideInUp")}>
-              Carbon is a cross-chain protocol that acts as<br />a building block for DeFi.
+            <Typography
+              color="textSecondary"
+              variant="body1"
+              className={clsx(
+                classes.subtitle,
+                "animate__animated animate__slideInUp"
+              )}
+            >
+              Carbon is a cross-chain protocol that acts as
+              <br />a building block for DeFi.
             </Typography>
           </FadeAndSlide>
-          <FadeAndSlide visible={inView}>
-            <div ref={scrollRef} className={clsx(classes.scrollContainer, { open: inView })} onClick={() => goToData()}>
-              <Scroll className={clsx(classes.scrollIcon, "bounce")} />
-              <Typography variant="body2" className={classes.scrollText}>SCROLL TO EXPLORE</Typography>
-            </div>
-          </FadeAndSlide>
+        </div>
+        <FadeAndSlide visible={inView} className={classes.scrollContainer}>
+          <div
+            ref={scrollRef}
+            className={clsx({ open: inView })}
+            onClick={() => goToData()}
+          >
+            <Scroll className={clsx(classes.scrollIcon, "bounce")} />
+            <Typography variant="body2" className={classes.scrollText}>
+              SCROLL TO EXPLORE
+            </Typography>
+          </div>
+        </FadeAndSlide>
+        <div className={classes.altCarbonContainer} ref={altCarbonRef}>
+          <Typography
+            variant="body1"
+            className={classes.altCarbon}
+            onClick={() => goToAltCarbon()}
+          >
+            Looking for Carbon DeFi by{" "}
+            <span className={classes.bancor}>Bancor</span>?
+          </Typography>
         </div>
       </Grid>
-    </div >
+    </div>
   );
 };
 
@@ -91,6 +142,7 @@ const useStyles = makeStyles((theme: Theme) => ({
     justifyContent: "center",
     alignItems: "center",
     position: "relative",
+    marginTop: "-2rem",
     [theme.breakpoints.down("sm")]: {
       marginLeft: "-24px",
       marginTop: "-24px",
@@ -99,7 +151,7 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
   headerContainer: {
     zIndex: 5,
-    marginTop: "10rem",
+    marginTop: "12rem",
     position: "absolute",
     top: 0,
     [theme.breakpoints.only("xs")]: {
@@ -134,15 +186,15 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
   scrollContainer: {
     opacity: 0,
-    marginTop: "30rem",
-    position: "relative",
+    top: "70vh",
+    position: "absolute",
     zIndex: 10,
     cursor: "pointer",
     "&.open": {
       opacity: 1,
     },
-    [theme.breakpoints.down("md")]: {
-      marginTop: "21.5rem",
+    [theme.breakpoints.down("xs")]: {
+      top: "60vh",
     },
   },
   scrollIcon: {
@@ -158,7 +210,36 @@ const useStyles = makeStyles((theme: Theme) => ({
     fontWeight: 600,
     color: theme.palette.text.hint,
   },
+  altCarbonContainer: {
+    top: "70vh",
+    position: "absolute",
+    width: "100%",
+    display: "flex",
+    justifyContent: "flex-end",
+    cursor: "pointer",
+    "@media (max-width: 790px)": {
+      marginTop: "0rem",
+    },
+  },
+  altCarbon: {
+    border: theme.palette.text.secondary,
+    background: theme.palette.background.default,
+    borderWidth: "1px",
+    borderStyle: "solid",
+    color: theme.palette.text.secondary,
+    borderRadius: "12px",
+    padding: theme.spacing(1, 1.5, 1, 1.5),
+    boxShadow:
+      "33px 33px 75px -10px #000000BF, -33px -33px 75px -10px #00000054",
+    "&:hover": {
+      color: theme.palette.primary.main,
+    },
+  },
   bodyTypography: {
     fontWeight: 400,
+  },
+  bancor: {
+    color: theme.palette.primary.main,
+    fontWeight: 700,
   },
 }));
