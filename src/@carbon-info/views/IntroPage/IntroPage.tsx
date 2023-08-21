63975f0b190eb99f31af600fbad5ec4131f3e548
Li Hui Cham <99931775+lihuicham@users.noreply.github.com>
2023-08-21 15:08:38 +0800

    Fix/Bancor Button Position (#55)
    * fix : bancor button position on large screens
    
    * Fix bancor button position
    
    * Fix alignment for powered by carbon section
    
    * Shift container for mobile devices
    
    ---------
    
    Co-authored-by: Sharmaine Cheong <88943108+sharmainec@users.noreply.github.com>

--- src/@carbon-info/views/IntroPage/IntroPage.tsx
+++ src/@carbon-info/views/IntroPage/IntroPage.tsx
@@ -85,7 +85,7 @@ const IntroPage: React.FC = () => {
           </FadeAndSlide>
         </div>
         <div className={classes.altCarbonContainer} ref={altCarbonRef}>
-          <Typography variant="body1" className={classes.altCarbon} onClick={() => goToAltCarbon()} >Looking for Carbon DeFi by <span className={classes.bancor}>Bancor</span>?</Typography>
+          <Typography variant="body2" className={classes.altCarbon} onClick={() => goToAltCarbon()} >Looking for Carbon DeFi by <span className={classes.bancor}>Bancor</span>?</Typography>
         </div>
       </Grid>
     </div >
@@ -167,14 +167,14 @@ const useStyles = makeStyles((theme: Theme) => ({
     color: theme.palette.text.hint,
   },
   altCarbonContainer: {
-    top: "70vh",
+    top: "calc(80vh - 30px)",
     position: "absolute",
     width: "100%",
     display: "flex",
     justifyContent: "flex-end",
     cursor: "pointer",
-    "@media (max-width: 790px)": {
-      marginTop: "0rem",
+    [theme.breakpoints.down("sm")]: {
+      marginRight: theme.spacing(3),
     },
   },
   altCarbon: {
@@ -184,7 +184,7 @@ const useStyles = makeStyles((theme: Theme) => ({
     borderStyle: "solid",
     color: theme.palette.text.secondary,
     borderRadius: "12px",
-    padding: theme.spacing(1, 1.5, 1, 1.5),
+    padding: theme.spacing(0.75, 1.25, 0.75, 1.25),
     boxShadow: "33px 33px 75px -10px #000000BF, -33px -33px 75px -10px #00000054",
     "&:hover": {
       color: theme.palette.primary.main,
